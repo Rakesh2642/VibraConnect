d7f6d7e72af9d078483c338315e50d9d9adbdce6
Bijjam Rakesh Reddy <95711036+Rakesh2642@users.noreply.github.com>
2023-11-26 13:09:27 +0530

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+
+import  ReactDOM  from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import  AuthProvider  from "./context/AuthContext";
+import { Queryprovider } from "./lib/react-query/queryprovider";
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+ <BrowserRouter>
+   <Queryprovider>
+      <AuthProvider>
+         <App />
+      </AuthProvider>
+   </Queryprovider>
+ </BrowserRouter>
+ 
+)
+
+ 
